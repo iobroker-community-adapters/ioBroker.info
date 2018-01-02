48289e0f92ea5bb70af5b73af659c28cbd6077d9
ldittmar <ldittmar.privat@gmail.com>
2018-01-02 21:25:18 +0100

    (ldittmar) translation

--- admin/words.js
+++ admin/words.js
@@ -1,33 +1,252 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
     "information adapter settings": {
-        "de": "Informationsadapter Einstellungen"
+        "de": "Informationsadapter Einstellungen",
+        "en": "Information adapter settings",
+        "nl": "Informatie adapter instellingen",
+        "it": "Impostazioni dell'adattatore di informazioni",
+        "fr": "Paramètres de l'adaptateur d'informations",
+        "pt": "Configurações do adaptador de informações",
+        "ru": "Настройки адаптера информации",
+        "es": "Configuración del adaptador de información"
     },
-    "Show ioBroker news:": {"de": "Zeige News vom ioBroker:"},
-    "Show forum news:": {"de": "Foren-News anzeigen:"},
+    "Show ioBroker news:": {
+        "de": "Zeige News vom ioBroker:",
+        "en": "Show ioBroker news:",
+        "pt": "Veja as novidades do ioBroker:",
+        "nl": "Ontdek wat er nieuw is in ioBroker:",
+        "it": "Mostra le notizie di ioBroker:",
+        "fr": "Afficher les nouvelles d'ioBroker:",
+        "ru": "Показать новости от ioBroker:",
+        "es": "Mostrar noticias do ioBroker:"
+    },
+    "Show forum news:": {
+        "de": "Foren-News anzeigen:",
+        "en": "Show forum news:",
+        "pt": "Mostrar notícias do fórum:",
+        "nl": "Toon forumnieuws:",
+        "it": "Mostra notizie sul forum:",
+        "fr": "Afficher les actualités du forum:",
+        "ru": "Показать новости форума:",
+        "es": "Mostrar noticias del foro:"
+    },
     "on save adapter restarts with new config immediately": {
         "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-        "ru": "При сохранении настроек адаптера он сразу же перезапускается"
+        "en": "On save adapter restarts with new config immediately.",
+        "pt": "Ao salvar as configurações dos adaptadores, será reiniciado imediatamente.",
+        "nl": "Bij het opslaan van instellingen worden de adapters onmiddellijk opnieuw gestart.",
+        "it": "Quando si salvano le impostazioni, gli adattatori verranno riavviati immediatamente.",
+        "fr": "Lors de la sauvegarde des paramètres, les adaptateurs seront immédiatement redémarrés.",
+        "es": "Al guardar la configuración de los adaptadores, se reiniciará de inmediato.",
+        "ru": "При сохранении настроек адаптера он сразу же перезапускается."
     },
-    "daysShortText": {"de": "T.", "en": "d."},
-    "can be done here": {"de": "können hier durchgeführt werden"},
-    "Updates": {"de": "Aktualisierungen"},
-    "New adapters": {"de": "Neue Adapter"},
-    "are listed here": {"de": "werden hier aufgelistet"},
-    "System informations": {"de": "Systeminformationen"},
-    "about the server": {"de": "über den Server"},
-    "adapters count": {"de": "Anzahl der Adapter"},
-    "Platform": {"de": "Betriebssystem"},
-    "Architecture": {"de": "Architektur"},
-    "Speed": {"de": "Geschwindigkeit"},
-    "System uptime": {"de": "System Betriebszeit"},
-    "Uptime": {"de": "Betriebszeit"},
-    "Run on background" : {"de": "Im Hintergrund laufen"},
-    "Installing adapter %s...": {"de": "Adapter %s wird installiert..."},
-    "The adapter %s has been successfully installed!": {"de": "Der Adapter %s wurde erfolgreich installiert!"},
-    "Failed to install %s": {"de": "Der Adapter %s konnte leider nicht intalliert werden."},
-    "Updating adapter %s...": {"de": "Adapter %s wird aktualisiert..."},
-    "The adpter %s has been successfully updated!":{"de": "Der Adapter %s wurde erfolgreich aktualisiert!"},
-    "Failed to update %s": {"de": "Der Adapter %s konnte leider nicht aktualisiert werden."},
-    "All adapters are up to date!": {"de": "Alle Adapter sind aktuell!"}
+    "daysShortText": {
+        "de": "T.", 
+        "en": "d.",
+        "pt": "d.",
+        "nl": "d.",
+        "it": "g.",
+        "fr": "j.",
+        "ru": "д.",
+        "es": "d."},
+    "can be done here": {
+        "de": "können hier durchgeführt werden",
+        "en": "can be done here",
+        "pt": "podem ser feitas aqui",
+        "nl": "kan hier gedaan worden",
+        "it": "può essere fatto qui",
+        "fr": "peut être fait ici",
+        "ru": "можно сделать здесь",
+        "es": "se puede hacer aquí"
+    },
+    "Updates": {
+        "de": "Aktualisierungen",
+        "en": "Updates",
+        "pt": "Atualizações",
+        "nl": "Updates",
+        "it": "Aggiornamenti",
+        "fr": "Mises à jour",
+        "ru": "обновления",
+        "es": "Actualizaciones"
+    },
+    "New adapters": {
+        "de": "Neue Adapter",
+        "en": "New adapters",
+        "pt": "Novos adaptadores",
+        "nl": "Nieuwe adapters",
+        "it": "Nuovi adattatori",
+        "fr": "Nouvelles cartes",
+        "ru": "Новые адаптеры",
+        "es": "Nuevos adaptadores"
+    },
+    "are listed here": {
+        "de": "werden hier aufgelistet",
+        "en": "are listed here",
+        "pt": "estão listados aqui",
+        "nl": "wordt hier vermeld",
+        "it": "sarà elencato qui",
+        "fr": "sera listé ici",
+        "ru": "перечислены здесь",
+        "es": "se enumeran aquí"
+    },
+    "System informations": {
+        "de": "Systeminformationen",
+        "en": "System informations",
+        "pt": "Informações do sistema",
+        "nl": "Systeeminformatie",
+        "it": "Informazioni di sistema",
+        "fr": "Informations système",
+        "ru": "Информационная система",
+        "es": "Información del sistema"
+    },
+    "about the server": {
+        "de": "über den Server",
+        "en": "about the server",
+        "pt": "sobre o servidor",
+        "nl": "over de server",
+        "it": "sul server",
+        "fr": "sur le serveur",
+        "ru": "над сервером",
+        "es": "sobre el servidor"
+    },
+    "adapters count": {
+        "de": "Anzahl der Adapter",
+        "en": "Adapters count",
+        "pt": "Número de adaptadores",
+        "nl": "Aantal adapters",
+        "it": "Numero di adattatori",
+        "fr": "Nombre d'adaptateurs",
+        "ru": "Количество адаптеров",
+        "es": "Número de adaptadores"
+    },
+    "Platform": {
+        "de": "Betriebssystem",
+        "en": "Platform",
+        "pt": "Sistema operacional",
+        "nl": "Besturingssysteem",
+        "it": "Sistema operativo",
+        "fr": "Système d'exploitation",
+        "ru": "операционная система",
+        "es": "Sistema operativo"
+    },
+    "Architecture": {
+        "de": "Architektur",
+        "en": "Architecture",
+        "pt": "Arquitetura",
+        "nl": "Architectuur",
+        "it": "Architettura",
+        "fr": "Architecture",
+        "ru": "Aрхитектура",
+        "es": "Arquitectura"
+    },
+    "Speed": {
+        "de": "Geschwindigkeit",
+        "en": "Speed",
+        "pt": "Velocidade",
+        "nl": "Snelheid",
+        "it": "Velocità",
+        "fr": "Vitesse",
+        "ru": "Cкорость",
+        "es": "Velocidad"
+    },
+    "System uptime": {
+        "de": "System Betriebszeit",
+        "en": "System uptime",
+        "pt": "Tempo de atividade (sis.)",
+        "nl": "Systeembedrijfstijd",
+        "it": "Tempo di attività (sis.)",
+        "fr": "Durée de fonctionnement (sys.)",
+        "ru": "Время работы системы",
+        "es": "Tiempo de actividad (sis.)"
+    },
+    "Uptime": {
+        "de": "Betriebszeit",
+        "en": "Uptime",
+        "pt": "Tempo de atividade",
+        "nl": "Bedrijfstijd",
+        "it": "Tempo di attività",
+        "fr": "Durée de fonctionnement",
+        "ru": "Провел",
+        "es": "Tiempo de actividad"
+    },
+    "Run on background" : {
+        "de": "Im Hintergrund laufen",
+        "en": "Run on background",
+        "pt": "Executar no fundo",
+        "nl": "Uitvoeren op achtergrond",
+        "it": "Esegui su sfondo",
+        "fr": "Exécuter en arrière-plan",
+        "ru": "Запуск на фоне",
+        "es": "Ejecutar en el fondo"
+    },
+    "Installing adapter %s...": {
+        "de": "Adapter %s wird installiert...",
+        "en": "Installing adapter %s...",
+        "pt": "O adaptador %s está sendo instalado...",
+        "nl": "Adapter %s wordt geïnstalleerd...",
+        "it": "Installazione dell'adattatore %s ...",
+        "fr": "L'adaptateur %s sera installé...",
+        "ru": "Установка адаптера %s ...",
+        "es": "Se está instalando el adaptador %s ..."
+    },
+    "The adapter %s has been successfully installed!": {
+        "de": "Der Adapter %s wurde erfolgreich installiert!",
+        "en": "The adapter %s has been successfully installed!",
+        "pt": "O adaptador %s foi instalado com sucesso!",
+        "nl": "De adapter %s is succesvol geïnstalleerd!",
+        "it": "L'adattatore %s è stato installato con successo!",
+        "fr": "L'adaptateur %s a été installé avec succès!",
+        "ru": "Адаптер %s успешно установлен!",
+        "es": "¡El adaptador %s se ha instalado correctamente!"
+    },
+    "Failed to install %s": {
+        "de": "Der Adapter %s konnte leider nicht intalliert werden.",
+        "en": "Failed to install %s",
+        "pt": "O adaptador %s, infelizmente, não pôde ser instalado.",
+        "nl": "De adapter %s kon niet worden geïnstalleerd.",
+        "it": "mpossibile installare l'adattatore %s.",
+        "fr": "L'adaptateur %s n'a pas pu être installé.",
+        "ru": "Адаптер %s к сожалению не может быть установлен.",
+        "es": "Desafortunadamente, el adaptador %s no se pudo instalar."
+    },
+    "Updating adapter %s...": {
+        "de": "Adapter %s wird aktualisiert...",
+        "en": "Updating adapter %s...",
+        "pt": "O adaptador %s está sendo atualizado...",
+        "nl": "",
+        "it": "",
+        "fr": "",
+        "ru": "Обновление адаптера %s ...",
+        "es": "Actualizando el adaptador %s ..."
+    },
+    "The adpter %s has been successfully updated!":{
+        "de": "Der Adapter %s wurde erfolgreich aktualisiert!",
+        "en": "The adapter %s has been successfully updated!",
+        "pt": "O adaptador %s foi atualizado com sucesso!",
+        "nl": "Adapter %s wordt bijgewerkt...",
+        "it": "L'adattatore %s viene aggiornato...",
+        "fr": "L'adaptateur %s est mis à jour...",
+        "ru": "Адаптер %s успешно обновлен!",
+        "es": "¡El adaptador %s se actualizó correctamente!"
+    },
+    "Failed to update %s": {
+        "de": "Der Adapter %s konnte leider nicht aktualisiert werden.",
+        "en": "Failed to update %s.",
+        "pt": "Desculpe, o adaptador %s não pôde ser atualizado.",
+        "nl": "Sorry, de adapter %s kon niet worden bijgewerkt.",
+        "it": "Mi spiace, l'adattatore %s non può essere aggiornato.",
+        "fr": "Désolé, l'adaptateur %s n'a pas pu être mis à jour.",
+        "ru": "Не удалось обновить %s.",
+        "es": "Lo sentimos, el adaptador %s no se pudo actualizar."
+    },
+    "All adapters are up to date!": {
+        "de": "Alle Adapter sind aktuell!",
+        "en": "All adapters are up to date!",
+        "pt": "Todos os adaptadores estão atualizados!",
+        "nl": "Alle adapters zijn up-to-date!",
+        "it": "Tutti gli adattatori sono aggiornati!",
+        "fr": "Tous les adaptateurs sont à jour!",
+        "ru": "Все адаптеры обновлены!",
+        "es": "¡Todos los adaptadores están actualizados!"
+    }
 };
