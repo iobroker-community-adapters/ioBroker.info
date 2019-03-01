bffe5cc2720c15d5e3f61c1a341633ca7828e0bc
ldittmar <ldittmar.privat@gmail.com>
2019-03-01 21:42:13 +0100

    Issues step 1

--- admin/lib/js/adapterIssues.js
+++ admin/lib/js/adapterIssues.js
@@ -2,11 +2,26 @@
 
 //------------------------------------------------------ SEARCH GITHUB --------------------------------------------------------------------
 
-function showIssues() {   
+function showIssues() {
 
     async function getIssues() {
+        const adapters = window.top.gMain.tabs.adapters.curInstalled;
+        if (adapters && typeof adapters === "object") {
+            await asyncForEach(Object.keys(adapters), async function (key) {
+                const adapter = adapters[key];
+                const $item = $('#forumEntryTemplate').children().clone(true, true);
+                $item.find('.label-success').remove();
+                
+                const full_name = adapter.readme.substring(adapter.readme.indexOf(".com/") + 5, adapter.readme.indexOf("/blob/"));
+                
+                $item.find('.titleLink').text(adapter.title).attr('href', "https://github.com/" + full_name + "/issues");
+                $item.find('.y_title').addClass('spoiler-content').css('padding-left', '20px');
+                $item.find('.y_content').addClass('spoiler-content').css('display', 'none').clear();
 
+                $('#adapterIssueList').append($item);
+            });
+        }
     }
-    
+
     getIssues();
 }
