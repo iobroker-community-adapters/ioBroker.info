7fb28c4284ab4f9d8c6b8c42c6e86f40f642cb49
ldittmar <ldittmar.privat@gmail.com>
2019-03-01 18:41:56 +0100

    really last try

--- admin/lib/js/adapterRequests.js
+++ admin/lib/js/adapterRequests.js
@@ -6,11 +6,17 @@ function showAdapterRequest() {
 
     async function getIssues() {
 
-        const issues = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=1&per_page=100")).json();
-        const issues2 = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=2&per_page=100")).json();
-        const issues3 = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=3&per_page=100")).json();
-        let allIssues = issues.concat(issues2);
-        allIssues = allIssues.concat(issues3);
+        let allIssues;
+        if (sessionStorage.getItem('ioBroker.info.adapterRequest')) {
+            allIssues = JSON.parse(sessionStorage.getItem('ioBroker.info.adapterRequest'));
+        } else {
+            const issues = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=1&per_page=100")).json();
+            const issues2 = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=2&per_page=100")).json();
+            const issues3 = await (await fetch("https://api.github.com/repos/ioBroker/AdapterRequests/issues?page=3&per_page=100")).json();
+            allIssues = issues.concat(issues2);
+            allIssues = allIssues.concat(issues3);
+            sessionStorage.setItem('ioBroker.info.adapterRequest', JSON.stringify(allIssues));
+        }
 
         await asyncForEach(allIssues, async function (issue) {
             const $item = $('#forumEntryTemplate').children().clone(true, true);
@@ -21,7 +27,7 @@ function showAdapterRequest() {
             $item.find('.byline').text(new Date(issue.created_at).toLocaleDateString('en', dateOptions) + " - " + issue.user.login);
             $item.find('.description').html(issue.body);
 
-            if (issue.assignee.login) {
+            if (issue.assignee && issue.assignee.login) {
                 $item.find('.assigner_img').attr('src', issue.assignee.avatar_url);
                 $item.find('.assigner').text('assined from ' + issue.assignee.login);
             } else {
