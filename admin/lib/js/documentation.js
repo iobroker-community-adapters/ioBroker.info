05ffab78e80858672e8b851376a5b44001716210
ldittmar <ldittmar.privat@gmail.com>
2019-03-03 21:43:51 +0100

    Now

--- admin/lib/js/documentation.js
+++ admin/lib/js/documentation.js
@@ -8,7 +8,7 @@ function showDocumentation() {
         if (!adapterConfig.doc_langs || adapterConfig.doc_langs.length === 0) {
             langs.push(systemLang);
             if (systemLang !== "en") {
-                langs.push("en");                
+                langs.push("en");
             }
             langs.push("noLang");
         } else {
@@ -34,43 +34,43 @@ function showDocumentation() {
         });
         langs = tmpLangs.split(',');
 
-        await asyncForEach(langs, async function (lang) {
-            await asyncForEach(infoData.docs.community[lang], async function (data) {
+        langs.forEach(function (lang) {
+            infoData.docs.community[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
                 $link.find('.forumClass').text(data.title);
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
                 const $li = $('<li/>').append($link);
                 $('#doc_community').append($li);
             });
-            await asyncForEach(infoData.docs.documentation[lang], async function (data) {
+            infoData.docs.documentation[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
                 $link.find('.forumClass').text(data.title);
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
                 const $li = $('<li/>').append($link);
                 $('#doc_documentation').append($li);
             });
-            await asyncForEach(infoData.docs.news[lang], async function (data) {
+            infoData.docs.news[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
-                $link.find('.forumClass').text(data.title);
+                $link.find('.forumClass').text(data.title + " (" + data.date + ")");
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
                 const $li = $('<li/>').append($link);
                 $('#doc_news').append($li);
             });
-            await asyncForEach(infoData.docs.blog[lang], async function (data) {
+            infoData.docs.blog[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
                 $link.find('.forumClass').text(data.title);
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
                 const $li = $('<li/>').append($link);
                 $('#doc_blog').append($li);
             });
-            await asyncForEach(infoData.docs.development[lang], async function (data) {
+            infoData.docs.development[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
                 $link.find('.forumClass').text(data.title);
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
                 const $li = $('<li/>').append($link);
                 $('#doc_development').append($li);
             });
-            await asyncForEach(infoData.docs.other[lang], async function (data) {
+            infoData.docs.other[lang].forEach(function (data) {
                 const $link = $('#tagTemplate').children().clone(true, true);
                 $link.find('.forumClass').text(data.title);
                 $link.find('.tag').addClass("tag label").attr("href", data.link);
