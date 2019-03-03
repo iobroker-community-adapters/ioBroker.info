00799744c00afc371461c43504f7cde1d650d957
ldittmar <ldittmar.privat@gmail.com>
2019-03-03 18:48:12 +0100

    Doc

--- admin/lib/js/helper.js
+++ admin/lib/js/helper.js
@@ -179,7 +179,7 @@ $(function () {
     });
 
     $(document.body).on('click', ".loadAdapterIssues", function () {
-        $(this).removeClass("loadAdapterIssues");        
+        $(this).removeClass("loadAdapterIssues");
         getAndWriteIssuesFor($(this).attr("data-adapter"));
     });
 
@@ -203,5 +203,43 @@ $(function () {
     $(document.body).on('click', '.spoiler-control', function () {
         $(this).parent().children(".spoiler-content").toggle();
     });
+
+
+    var $menu = $('#sidebar-wrapper');
+    var $content = $('#main-wrapper');
+
+    $content.addClass('no-transition');
+    $menu.hide();
+    $menu.css('right', -($menu.outerWidth() + 10));
+    $content.removeClass('col-md-10').addClass('col-md-12');
+
+    $('#documentation').click(function () {
+        $content.removeClass('no-transition');
+        if ($menu.is(':visible') && $content.hasClass('col-md-10')) {
+            // Slide out
+            $menu.animate({
+                right: -($menu.outerWidth() + 10)
+            }, function () {
+                $menu.hide(1000);
+            });
+            $content.removeClass('col-md-10').addClass('col-md-12');
+            $.cookie('offcanvas', 'hide');
+        } else {
+            // Slide in
+            $menu.show(500).animate({right: 0});
+            $content.removeClass('col-md-12').addClass('col-md-10');
+            $.cookie('offcanvas', 'show');
+        }
+        if ($content.hasClass('col-md-12') && $menu.is(':hidden')) {
+            $menu.animate({
+                right: 0
+            }, function () {
+                $menu.show(1000);
+            });
+            $content.removeClass('no-transition');
+            $content.removeClass('col-md-12').addClass('col-md-10');
+        }
+    });
+    $('.bs-tooltip').tooltip();
 
 });
