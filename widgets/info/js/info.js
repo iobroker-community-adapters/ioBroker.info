6815ece4c1fb684bdd663ceb57248febdb01a61a
ldittmar81 <ldittmar.privat@gmail.com>
2019-03-13 15:20:26 +0100

    vis widget first try

---
+++ widgets/info/js/info.js
@@ -0,0 +1,48 @@
+/* global systemDictionary, vis */
+
+"use strict";
+
+// add translations for edit mode
+$.get( "adapter/info/words.js", function(script) {
+    let translation = script.substring(script.indexOf('{'), script.length);
+    translation = translation.substring(0, translation.lastIndexOf(';'));
+    $.extend(systemDictionary, JSON.parse(translation));
+});
+
+vis.binds["info"] = {
+    version: "0.0.1",
+    showVersion: function () {
+        if (vis.binds["info"].version) {
+            console.log('Version template: ' + vis.binds["info"].version);
+            vis.binds["info"].version = null;
+        }
+    },
+    createWidget: function (widgetID, view, data, style) {
+        var $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds["info"].createWidget(widgetID, view, data, style);
+            }, 100);
+        }
+
+        var text = '';
+        text += 'OID: ' + data.oid + '</div><br>';
+        text += 'OID value: <span class="myset-value">' + vis.states[data.oid + '.val'] + '</span><br>';
+        text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + '</span><br>';
+        text += 'extraAttr: ' + data.extraAttr + '<br>';
+        text += 'Browser instance: ' + vis.instance + '<br>';
+        text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || '') + '</textarea><br>';
+
+        $('#' + widgetID).html(text);
+
+        // subscribe on updates of value
+        if (data.oid) {
+            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                $div.find('.info-value').html(newVal);
+            });
+        }
+    }
+};
+
+vis.binds["info"].showVersion();
