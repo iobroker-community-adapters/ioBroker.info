ff890101d52cd899bc3fe194a4a3959dc71e375f
ldittmar81 <ldittmar.privat@gmail.com>
2019-03-19 11:35:44 +0100

    CPU and Disk

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const axios = require('axios');
 const sistm = require('systeminformation');
 
+const cpuUsed = [];
+const memUsed = [];
+const fsUsed = {};
+
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
@@ -144,6 +148,17 @@ const updateSysinfo = function () {
             })
             .catch(error => adapter.log.error(error));
 
+    sistm.currentLoad()
+            .then(data => {
+                Object.keys(data).forEach(function (key) {
+                    if (typeof data[key] === "number") {
+                        setState('cpu', 'currentLoad', key, typeof data[key], data[key]);
+                    }
+                });
+                setInterval(updateCurrentCPUInfos, 2000);
+            })
+            .catch(error => adapter.log.error(error));
+
     //MEMORY
     sistm.mem()
             .then(data => {
@@ -175,6 +190,81 @@ const updateSysinfo = function () {
             })
             .catch(error => adapter.log.error(error));
 
+    //DISKS
+    sistm.blockDevices()
+            .then(data => {
+                Object.keys(data).forEach(function (key) {
+                    if (data.length > 0) {
+                        Object.keys(data[key]).forEach(function (key2) {
+                            if ((typeof data[key][key2] === 'string' && data[key][key2].length) || typeof data[key][key2] !== 'string') {
+                                setState('disks', 'blockDevices', "dev_" + key + "_" + key2, typeof data[key][key2], data[key][key2]);
+                            }
+                        });
+                    }
+                });
+            })
+            .catch(error => console.error(error));
+
+    sistm.diskLayout()
+            .then(data => {
+                if (data.length > 0) {
+                    Object.keys(data).forEach(function (key) {
+                        Object.keys(data[key]).forEach(function (key2) {
+                            if ((typeof data[key][key2] === 'string' && data[key][key2].length) || typeof data[key][key2] !== 'string') {
+                                setState('disks', 'diskLayout', "dev_" + key + "_" + key2, typeof data[key][key2], data[key][key2]);
+                            }
+                        });
+                    });
+                }
+            })
+            .catch(error => console.error(error));
+
+    sistm.fsSize()
+            .then(data => {
+                if (data.length > 0) {
+                    Object.keys(data).forEach(function (key) {
+                        fsUsed[key] = [];
+                        Object.keys(data[key]).forEach(function (key2) {
+                            if ((typeof data[key][key2] === 'string' && data[key][key2].length) || typeof data[key][key2] !== 'string') {
+                                setState('disks', 'fsSize', "fs_" + key + "_" + key2, typeof data[key][key2], data[key][key2]);
+                            }
+                            if (key2 === "used") {
+                                setState('disks', 'fsSize', "fs_" + key + "_used_hist", "array", "[]");
+                            }
+                        });
+                    });
+                    setInterval(updateCurrentFilesystemInfos, 5000);
+                }
+            })
+            .catch(error => console.error(error));
+
+
+};
+
+const updateCurrentCPUInfos = function () {
+
+    sistm.mem()
+            .then(data => {
+                adapter.setState('sysinfo.cpu.currentLoad_avgload', {val: data['avgload'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload', {val: data['currentload'], ack: true});
+                cpuUsed.push(data['currentload']);
+                if (cpuUsed.length > 30) {
+                    cpuUsed.shift();
+                }
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_hist', {val: cpuUsed.toString(), ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_user', {val: data['currentload_user'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_system', {val: data['currentload_system'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_nice', {val: data['currentload_nice'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_idle', {val: data['currentload_idle'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_irq', {val: data['currentload_irq'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_raw_currentload', {val: data['raw_currentload'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_raw_currentload_user', {val: data['raw_currentload_user'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_raw_currentload_system', {val: data['raw_currentload_system'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_raw_currentload_nice', {val: data['raw_currentload_nice'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_raw_currentload_idle', {val: data['raw_currentload_idle'], ack: true});
+                adapter.setState('sysinfo.cpu.currentLoad_currentload_irq', {val: data['currentload_irq'], ack: true});
+            })
+            .catch(error => adapter.log.error(error));
 
 };
 
@@ -184,6 +274,11 @@ const updateCurrentMemoryInfos = function () {
             .then(data => {
                 adapter.setState('sysinfo.memory.mem_free', {val: data['free'], ack: true});
                 adapter.setState('sysinfo.memory.mem_used', {val: data['used'], ack: true});
+                memUsed.push(data['used']);
+                if (memUsed.length > 30) {
+                    memUsed.shift();
+                }
+                adapter.setState('sysinfo.memory.mem_used_hist', {val: memUsed.toString(), ack: true});
                 adapter.setState('sysinfo.memory.mem_active', {val: data['active'], ack: true});
                 adapter.setState('sysinfo.memory.mem_available', {val: data['available'], ack: true});
                 adapter.setState('sysinfo.memory.mem_buffcache', {val: data['buffcache'], ack: true});
@@ -191,6 +286,26 @@ const updateCurrentMemoryInfos = function () {
                 adapter.setState('sysinfo.memory.mem_swapfree', {val: data['swapfree'], ack: true});
             })
             .catch(error => adapter.log.error(error));
+
+};
+
+const updateCurrentFilesystemInfos = function () {
+
+    sistm.fsSize()
+            .then(data => {
+                if (data.length > 0) {
+                    Object.keys(data).forEach(function (key) {
+                        adapter.setState('sysinfo.disks.fsSize_fs_' + key + '_used', {val: data[key]['used'], ack: true});
+                        fsUsed[key].push(data[key]['used']);
+                        if (fsUsed[key].length > 12) {
+                            fsUsed[key].shift();
+                        }
+                        adapter.setState('sysinfo.disks.fsSize_fs_' + key + '_used_hist', {val: fsUsed[key].toString(), ack: true});
+                        adapter.setState('sysinfo.disks.fsSize_fs_' + key + '_use', {val: data[key]['use'], ack: true});
+                    });
+                }
+            })
+            .catch(error => console.error(error));
 
 };
 
