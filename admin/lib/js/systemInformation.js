fde8c60e2dace8d152a58a3ad4c00fdaef0f430c
ldittmar81 <ldittmar.privat@gmail.com>
2019-03-21 13:31:59 +0100

    Systemdata (modal)

--- admin/lib/js/systemInformation.js
+++ admin/lib/js/systemInformation.js
@@ -5,8 +5,8 @@ const memLabels = [];
 const diskLabels = [];
 
 function startCharts() {
-    
-    if(cpuLabels.length === 0){
+
+    if (cpuLabels.length === 0) {
         let labelText = 0;
         for (let i = 0; i < 31; i++) {
             if (labelText % 5 === 0) {
@@ -18,7 +18,7 @@ function startCharts() {
         }
         cpuLabels.reverse();
     }
-    if(memLabels.length === 0){
+    if (memLabels.length === 0) {
         let labelText = 0;
         for (let i = 0; i < 31; i++) {
             if (labelText % 5 === 0) {
@@ -30,7 +30,7 @@ function startCharts() {
         }
         memLabels.reverse();
     }
-    if(diskLabels.length === 0){
+    if (diskLabels.length === 0) {
         let labelText = 0;
         for (let i = 0; i < 31; i++) {
             if (labelText % 5 === 0) {
@@ -45,7 +45,7 @@ function startCharts() {
 
     socket.emit('subscribe', 'info.0.sysinfo.cpu.currentLoad_currentload_hist');
     socket.emit('subscribe', 'info.0.sysinfo.memory.mem_used_hist');
-    
+
     socket.on('stateChange', function (id, obj) {
         if (id === "info.0.sysinfo.cpu.currentLoad_currentload_hist") {
             infoCharts.showCPU(obj.val.split(','));
@@ -57,12 +57,12 @@ function startCharts() {
             infoCharts.showCPU(data.val.split(','));
         }
     });
-    
+
 }
 
 const infoCharts = {
-    showCPU: function(data) {
-      
+    showCPU: function (data) {
+
         var data = {
             labels: cpuLabels,
             series: [data]
@@ -81,12 +81,44 @@ const infoCharts = {
 };
 
 const systemInformations = {
-    getData: function(){
-        socket.emit('getObjectView', 'info', 'systemdata', {startkey: 'info.0.sysinfo.', endkey: 'info.0.sysinfo.\u9999'}, function (err, res) {
-        if (!err && res) {
-           
-        }
-    });
+    getData: function () {
+        socket.emit('getObjectView', 'system', 'state', {startkey: 'info.0.sysinfo.', endkey: 'info.0.sysinfo.\u9999'}, function (err, res) {
+            if (!err && res) {
+                var _res = {};
+                for (var i = 0; i < res.rows.length; i++) {
+                    const link = res.rows[i].id.split('.');
+                    _res[res.rows[i].id] = {};
+                    _res[res.rows[i].id].systype = link[3];
+                    if (link.length > 5) {
+                        _res[res.rows[i].id].syssubtype = link[4];
+                    }
+                    if (link.length > 6) {
+                        _res[res.rows[i].id].device = link[5];
+                    }
+                    _res[res.rows[i].id].name = res.rows[i].value.common.name;
+                    _res[res.rows[i].id].unit = res.rows[i].value.common.unit;
+                    _res[res.rows[i].id].min = res.rows[i].value.common.min;
+                    _res[res.rows[i].id].max = res.rows[i].value.common.max;
+                }
+
+                Object.keys(_res).forEach(function (key) {
+                    socket.emit('getState', key, function (err, data) {
+                        if (!err && data) {
+                            const obj = _res[key];
+                            obj.value = data;
+                            systemInformations.writeData(obj);
+                        }
+                    });
+                });
+
+            } else {
+                console.log(JSON.stringify(err));
+            }
+        });
+    },
+    writeData: function (obj) {
+        const row = "<dt>" + _(obj.name) + "</dt><dd>" + obj.value + "</dd>";
+        $('#sys_info_' + obj.systype + (obj.systype !== "battery" ? '_' + obj.syssubtype : '')).append(row);
     }
 };
 
