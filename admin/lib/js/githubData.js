345a90c0cb07fcfa57866a40b68d1f2d1c50a5a8
ldittmar81 <ldittmar.privat@gmail.com>
2019-04-09 17:23:53 +0200

    Fix

--- admin/lib/js/githubData.js
+++ admin/lib/js/githubData.js
@@ -237,7 +237,7 @@ query{
                         login
                     }
                     createdAt
-                    $reaction
+                    $reactions
                 }
                 cursor
             }
